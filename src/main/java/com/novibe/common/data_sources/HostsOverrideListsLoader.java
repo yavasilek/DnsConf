ad25bbb7c3cb5e312be617efbcc5e415f2065ba4
yavasilek <yavasilek@gmail.com>
2026-04-19 16:12:16 +0300

    Enhance mapLine method with null checks
    Add null checks for delimiter and website in mapLine method.

--- src/main/java/com/novibe/common/data_sources/HostsOverrideListsLoader.java
+++ src/main/java/com/novibe/common/data_sources/HostsOverrideListsLoader.java
@@ -19,7 +19,8 @@ public class HostsOverrideListsLoader extends ListLoader<HostsOverrideListsLoade
                 .filter(str -> !str.isBlank())
                 .filter(line -> !line.startsWith("#"))
                 .filter(line -> !HostsBlockListsLoader.isBlock(line))
-                .map(this::mapLine);
+                .map(this::mapLine)
+                .filter(route -> route != null);
     }
 
     @Override
@@ -28,10 +29,12 @@ public class HostsOverrideListsLoader extends ListLoader<HostsOverrideListsLoade
     }
 
     private BypassRoute mapLine(String line) {
-        int delimiter = line.indexOf(" ");
-        String ip = line.substring(0, delimiter++);
-        String website = line.substring(delimiter);
-        return new BypassRoute(ip, website);
-    }
+    int delimiter = line.indexOf(" ");
+    if (delimiter == -1) return null;
+    String ip = line.substring(0, delimiter);
+    String website = line.substring(delimiter + 1).strip();
+    if (website.isEmpty()) return null;
+    return new BypassRoute(ip, website);
+}
 
 }
